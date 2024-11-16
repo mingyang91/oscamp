ea7f07a77f159867373442349b62fca0ffda2ffa
明扬 <my@famer.me>
2024-11-16 22:45:24 +0800

    rollback

--- arceos/scripts/make/utils.mk
+++ arceos/scripts/make/utils.mk
@@ -23,12 +23,10 @@ define make_disk_image
 endef
 
 define mk_pflash
-  @RUSTFLAGS="" cargo build -p origin --target riscv64gc-unknown-none-elf --release
+  @RUSTFLAGS="" cargo build -p origin  --target riscv64gc-unknown-none-elf --release
   @rust-objcopy --binary-architecture=riscv64 --strip-all -O binary ./target/riscv64gc-unknown-none-elf/release/origin /tmp/origin.bin
   @printf "pfld\00\00\00\01" > /tmp/prefix.bin
-  $(if $(filter Darwin,$(shell uname)), \
-    @printf "%08x" `stat -f "%z" /tmp/origin.bin` | xxd -r -ps > /tmp/size.bin, \
-    @printf "%08x" `stat -c "%s" /tmp/origin.bin` | xxd -r -ps > /tmp/size.bin)
+  @printf "%08x" `stat -c "%s" /tmp/origin.bin` | xxd -r -ps > /tmp/size.bin
   @cat /tmp/prefix.bin /tmp/size.bin > /tmp/head.bin
   @dd if=/dev/zero of=./$(1) bs=1M count=32
   @dd if=/tmp/head.bin of=./$(1) conv=notrunc
