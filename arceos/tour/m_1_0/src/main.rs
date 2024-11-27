ae6ab28421b6db762fa87267cb9c6ceb4c53af75
明扬 <my@famer.me>
2024-11-27 20:25:31 +0800

    handle page fault

--- arceos/tour/m_1_0/src/main.rs
+++ arceos/tour/m_1_0/src/main.rs
@@ -1,29 +1,42 @@
+#![feature(new_uninit)]
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
+extern crate alloc;
 #[cfg(feature = "axstd")]
 extern crate axstd as std;
-extern crate alloc;
 
 #[macro_use]
 extern crate axlog;
 
-mod task;
-mod syscall;
 mod loader;
+mod syscall;
+mod task;
 
-use axstd::io;
-use axhal::paging::MappingFlags;
+use alloc::sync::{Arc, Weak};
 use axhal::arch::UspaceContext;
 use axhal::mem::VirtAddr;
+use axhal::paging::MappingFlags;
+use axhal::trap::{register_trap_handler, PAGE_FAULT};
+use axmm::{kernel_aspace, AddrSpace};
+use axstd::io;
 use axsync::Mutex;
-use alloc::sync::Arc;
-use axmm::AddrSpace;
+use core::mem::MaybeUninit;
 use loader::load_user_app;
 
 const USER_STACK_SIZE: usize = 0x10000;
 const KERNEL_STACK_SIZE: usize = 0x40000; // 256 KiB
 const APP_ENTRY: usize = 0x1000;
+
+#[register_trap_handler(PAGE_FAULT)]
+fn handle_page_fault(vaddr: VirtAddr, flags: MappingFlags, is_user: bool) -> bool {
+    unsafe { USPACE.upgrade() }
+        .expect("No user address space!")
+        .lock()
+        .handle_page_fault(vaddr, flags | MappingFlags::USER)
+}
+
+static mut USPACE: Weak<Mutex<AddrSpace>> = Weak::new();
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
@@ -36,14 +49,14 @@ fn main() {
     }
 
     // Init user stack.
-    let ustack_top = init_user_stack(&mut uspace, true).unwrap();
+    let ustack_top = init_user_stack(&mut uspace, false).unwrap();
     ax_println!("New user address space: {:#x?}", uspace);
 
+    let aspace = Arc::new(Mutex::new(uspace));
+    unsafe { USPACE = Arc::downgrade(&aspace) };
+
     // Let's kick off the user process.
-    let user_task = task::spawn_user_task(
-        Arc::new(Mutex::new(uspace)),
-        UspaceContext::new(APP_ENTRY.into(), ustack_top),
-    );
+    let user_task = task::spawn_user_task(aspace, UspaceContext::new(APP_ENTRY.into(), ustack_top));
 
     // Wait for user process to exit ...
     let exit_code = user_task.join();
@@ -55,13 +68,16 @@ fn init_user_stack(uspace: &mut AddrSpace, populating: bool) -> io::Result<VirtA
     let ustack_vaddr = ustack_top - crate::USER_STACK_SIZE;
     ax_println!(
         "Mapping user stack: {:#x?} -> {:#x?}",
-        ustack_vaddr, ustack_top
-    );
-    uspace.map_alloc(
         ustack_vaddr,
-        crate::USER_STACK_SIZE,
-        MappingFlags::READ | MappingFlags::WRITE | MappingFlags::USER,
-        populating,
-    ).unwrap();
+        ustack_top
+    );
+    uspace
+        .map_alloc(
+            ustack_vaddr,
+            crate::USER_STACK_SIZE,
+            MappingFlags::READ | MappingFlags::WRITE | MappingFlags::USER,
+            populating,
+        )
+        .unwrap();
     Ok(ustack_top)
 }
